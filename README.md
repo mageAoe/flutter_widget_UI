25201eccffc3ee43d3e1f914b2cf184e65766a60
mageAoe <scq1144282310@163.com>
2023-11-10 21:22:53 +0800

    update readme

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 # flutter_widget_ui
 
-A new Flutter project.
+## Login
 
-## Getting Started
+<img src="./assets/README/Login_02.png" width="234" />
+<img src="./assets/README/Login_01.png" width="234" />
 
-This project is a starting point for a Flutter application.
+## sushi_shop_application
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+<img src="./assets/README/sushi_shop_01.png" width="234" />
+<img src="./assets/README/sushi_shop_03.png" width="234" />
+<img src="./assets/README/sushi_shop_02.png" width="234" />
+<img src="./assets/README/sushi_shop_04.png" width="234" />
--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,25 @@ import 'package:flutter/material.dart';
 
 // import './widget/AnimatedBottomBar/BottomNavigationBar.dart';
 // import './Animation/lottie_start_animation/index.dart';
-import 'application/beverage_recipe_application/main.dart';
+// import 'application/beverage_recipe_application/main.dart';
 
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
 
 // 上面的导入使用上面main函数启动
 // 下面这个比较特殊
-// import 'package:provider/provider.dart';
-// import './application/sushi_shop_application/main.dart';
-// import 'package:flutter_widget_ui/application/sushi_shop_application/models/shop.dart';
+import 'package:provider/provider.dart';
+import './application/sushi_shop_application/main.dart';
+import 'package:flutter_widget_ui/application/sushi_shop_application/models/shop.dart';
 
-// void main() {
-//   runApp(
-//     ChangeNotifierProvider(
-//       create: (context) => Shop(),
-//       child: const MyApp(),
-//     )
-//   );
-// }
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Shop(),
+      child: const MyApp(),
+    )
+  );
+}
